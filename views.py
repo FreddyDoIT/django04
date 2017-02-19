7aca737f488f4e6c3e705b7d847c2d3f474dc77a
freddy <freddydoit@163.com>
2017-02-19 23:00:17 +0800

    add all new files

--- views.py
+++ views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    html = '''
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>论坛首页</title>
+        </head>
+        <body>
+            <h2>Python部落论坛</h2>
+            <div>运维专区</div>
+            <div>Django专区</div>
+            <div>部落建设</div>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
