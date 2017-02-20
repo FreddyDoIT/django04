dad3a0ab703ea959d094f7502b827d2e0471a218
freddy <freddydoit@163.com>
2017-02-20 14:53:20 +0800

    modified views.py and its related files

--- views.py
+++ views.py
@@ -1,20 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    html = '''
-    <!doctype html>
-    <html>
-        <head>
-            <meta charset="utf-8" />
-            <title>论坛首页</title>
-        </head>
-        <body>
-            <h2>Python部落论坛</h2>
-            <div>运维专区</div>
-            <div>Django专区</div>
-            <div>部落建设</div>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+
+    return render(request,"index.html")
 
