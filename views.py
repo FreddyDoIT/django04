771e0810d95f3726995f7d859cfaf4e161785907
freddy <freddydoit@163.com>
2017-02-21 15:10:01 +0800

    modify the models by using for method

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,"index.html")
+    block_info=[{"name":"人间词话","descp":"好文章投过来","admin":"管理员：Freddy"},
+                {"name":"工作","descp":"公司列表","admin":"管理员：Freddy"},
+               {"name":"学校","descp":"人脉网络","admin":"管理员：Freddy"},]
+    return render(request,"index.html",{"blocks":block_info})
 
