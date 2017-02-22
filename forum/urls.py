19e6c64f3f8ff4cb936aa7eeb416558164f39af4
freddy <freddydoit@163.com>
2017-02-22 17:42:35 +0800

    add a new form

--- forum/urls.py
+++ forum/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 import django
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
+    url(r'^article/', include('article.urls')),
 ]
