c74117122580cd7b2e997643c74694b66a273b55
freddy <freddydoit@163.com>
2017-02-24 14:34:21 +0800

    finished the article_create form completely

--- article/views.py
+++ article/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from block.models import Block
 from .models import Article
+from django.shortcuts import redirect
+
 
 def article_list(request, block_id):
     block_id = int(block_id)
-    block = Block.objects.get(id = block_id)
-    articles_objs = Article.objects.filter(block=block, status = 0).order_by("-id")
-    return render(request, "article_list.html", {"articles": articles_objs, "b":block})
+    block = Block.objects.get(id=block_id)
+    articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
+    return render(request, "article_list.html", {"articles": articles_objs, "b": block})
+
 
 def article_create(request, block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
-    articles_objs = Article.objects.filter(block=block, status = 0).order_by("-id")
-    return render(request, "article_create.html", {"articles": articles_objs,"b":block})
+    if request.method == "GET":
+        return render(request, "article_create.html", { "b": block})
+
+    else:
+        title = request.POST["title"].strip()
+        content = request.POST["content"].strip()
+        if not title or not content:
+            return render(request,"article_create.html",
+                          {"b":block, "error":"标题和内容都不得为空.","title":title,"content":content})
+        if len(title)>100 or len(content)>10000:
+            return render(request,"article_create.html",
+                          {"b":block, "error":"标题或内容太长了.","title":title,"content":content})
+
+        article = Article(block=block, title=title, content=content,status=0)
+        article.save()
+        return redirect("/article/list/%s" % block_id)
